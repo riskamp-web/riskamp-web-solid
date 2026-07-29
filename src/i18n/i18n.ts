0c170aebb23b385e83d9dfa45b91bcc13aba7607
duncan <dwerner@riskamp.com>
2026-07-29 12:11:01 -0700

    add locale to i18n lib

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,12 +1,16 @@
 
+import { createSignal } from 'solid-js';
 import { createStore } from 'solid-js/store';
 import en from '~/i18n/lang/en';
 
 export type I18N = typeof en;
 
-export const [i18n_instance, setI18nInstance] = createStore({
+const [i18n_instance, setI18nInstance] = createStore({
   strings: en as I18N,
 });
+
+const [currentLocale, setCurrentLocale] = createSignal('en-us');
+export { currentLocale };
 
 export function t(key?: keyof I18N) {
   return key ? i18n_instance.strings[key] : '';
@@ -26,20 +30,43 @@ export function format(text: string, values: Record<string, string | number>): s
   return text.replace(/\{(\w+)\}/g, (all, key) => key in values ? String(values[key]) : all);
 }
 
+/**
+ * updating this method to take a proper locale (e.g. en-us). we'll 
+ * pull the language based on the first half.
+ * 
+ * @param locale 
+ */
 export async function UpdateLanguage(locale?: string) {
+
   // FIXME: we need to update the sheet language as well...
-  if (locale && locale !== 'en') {
-    locale = locale.toLowerCase();
-    const data = (await import(`~/i18n/lang/${locale}.ts`)).default;
-    console.info(`assigning from ${locale}`, { data });
-    // start with the base, in case anything is missing, then apply the deltas
-    setI18nInstance('strings', { ...en, ...data });
-    // some things (specific example: the insert function button, which
-    // is inserted as static html and not managed by solid) cannot react
-    // to changes in the managed language object. so we need to fire
-    // an event for anyone who needs to take specific action.
-    requestAnimationFrame(() => window.dispatchEvent(new CustomEvent('update-language')));
-  } else {
-    setI18nInstance('strings', en);
+
+  // TODO: validate
+
+  if (locale) {
+
+    setCurrentLocale(locale);
+    const lang = locale.substring(0, 2).toLowerCase();
+
+    if (lang !== 'en') {
+
+      const data = (await import(`~/i18n/lang/${lang}.ts`)).default;
+      console.info(`assigning from ${lang}`, { data });
+
+      // start with the base, in case anything is missing, then apply the deltas
+      setI18nInstance('strings', { ...en, ...data });
+
+      // some things (specific example: the insert function button, which
+      // is inserted as static html and not managed by solid) cannot react
+      // to changes in the managed language object. so we need to fire
+      // an event for anyone who needs to take specific action.
+      requestAnimationFrame(() => window.dispatchEvent(new CustomEvent('update-language')));
+    } else {
+
+      setCurrentLocale('en-us');
+      setI18nInstance('strings', en);
+
+    }
+
   }
 }
+
