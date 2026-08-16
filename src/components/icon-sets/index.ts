d3e80a82f4590ed676625fccdf111c4c85968526
duncan <dwerner@riskamp.com>
2026-08-16 13:49:28 -0700

    clean up icons
    going back to the days when we were integrating different icon sets,
    there was a lot of plumbing around defining and using sets. that
    plumbing is still there, but I removed everything except the current,
    bespoke icon set. we can simplify a little futher but there's no hurry

--- src/components/icon-sets/index.ts
+++ src/components/icon-sets/index.ts
@@ -1,6 +1,4 @@
 
-// import { icons as mixed } from './mixed';
-// import { icons as tabler } from './tabler';   // WIP -- needs fluent added to s5-icon-lib
 import { icons as custom } from './custom';
 
 /** The active icon set. Swap the assignment to try another mapping. */
